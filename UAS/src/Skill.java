ea2c6d60d32b4437170f3d268b00bbc59f27a59a
Pranarendra08 <92442022+Pranarendra08@users.noreply.github.com>
2021-12-02 17:49:09 +0700

    Delete UAS directory

--- UAS/src/Skill.java
+++
@@ -1,7 +0,0 @@
-public class Skill {
-    // tiap tipe hero punya skill yang berbeda
-    // tank, Punch kick guard
-    // fighter, punch slash
-    // assasin, slash kick
-    // mage, heal, Thunderbolt
-}
